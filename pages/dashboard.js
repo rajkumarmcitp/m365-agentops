325153b4abf2e2d01f36cead76809c6672a21ae8
Rajkumar <rajkumar.mcitp@gmail.com>
2026-07-09 17:50:38 +0530

    Fix: import showToast function for Zero Trust assessment messages

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,6 +2,7 @@ import { go, state } from '../app.js'
 import { getDevices, getUsers, getSecurityScore, callAPI, getMessageCenterMessages, getServiceHealth } from '../lib/api-client.js'
 import { isDemoAccount } from '../lib/demo-account.js'
 import { MC_MESSAGES, SVC_HEALTH, SVC_META } from '../data/msgcenter-data.js'
+import { showToast } from '../components/toast.js'
 
 let realDeviceCount = 0
 let realUserCount = 0
